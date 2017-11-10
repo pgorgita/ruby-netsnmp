e9f8bfb90303de1b0a8a27264385852d7679b9ba
Paulo Gorgita <paulo.gorgita@swisscom.com>
2017-11-10 11:25:16 +0100

    cosmetic fix

--- lib/netsnmp/client.rb
+++ lib/netsnmp/client.rb
@@ -129,7 +129,7 @@ module NETSNMP
       begin
         yield
       rescue Timeout::Error => e
-        raise e if retries == 0
+        raise e unless retries > 0
         retries -= 1
         retry
       end
