5b7821836a64be637396c826c7400193e26a82cf
Paulo Gorgita <paulo.gorgita@swisscom.com>
2017-11-10 11:23:52 +0100

    common steps for all request types extracted into #xet method now called by any request. Returns now always a Hash oid to value

--- lib/netsnmp/client.rb
+++ lib/netsnmp/client.rb
@@ -47,56 +47,44 @@ module NETSNMP
 
     # Performs an SNMP GET Request
     #
-    # @see {NETSNMP::Varbind#new}
-    #
-    def get(oid_opts)
-      request = @session.build_pdu(:get, oid_opts)
-      response = handle_retries { @session.send(request) }
-      yield response if block_given?
-      response.varbinds.first.value
+    def get(oid_opts, &block)
+      xet(oid_opts, :get, &block)
     end
 
     # Performs an SNMP GETNEXT Request
     # 
-    # @see {NETSNMP::Varbind#new}
+    def get_next(oid_opts, &block)
+      xet(oid_opts, :getnext, &block)
+    end
+
+    # Perform a SNMP SET Request
     #
-    def get_next(oid_opts)
-      request = @session.build_pdu(:getnext, oid_opts)
-      response = handle_retries { @session.send(request) }
-      yield response if block_given?
-      varbind = response.varbinds.first
-      [varbind.oid, varbind.value]
+    def set(oid_opts, &block)
+      xet(oid_opts, :set, &block)
     end
 
     # Perform a SNMP Walk (issues multiple subsequent GENEXT requests within the subtree rooted on an OID)
     #
     # @param [String] oid the root oid from the subtree
     #
-    # @return [Enumerator] the enumerator-collection of the oid-value pairs
+    # @return [Hash] the enumerator-collection of the oid-value pairs
     #
-    def walk(oid: )
+    def walk(oid: , &block)
+      output = {}
       walkoid = oid
-      Enumerator.new do |y|
-        code = walkoid
-        first_response_code = nil
-        catch(:walk) do
-          loop do
-            get_next(oid: code) do |response|
-              response.varbinds.each do |varbind|
-                code = varbind.oid
-                if !OID.parent?(walkoid, code) or 
-                    varbind.value.eql?(:endofmibview) or
-                    code == first_response_code
-                  throw(:walk)             
-                else
-                  y << [code, varbind.value]
-                end
-                first_response_code ||= code
-              end
-            end
-          end
+      req_type = :get
+      loop do
+        next_get = xet({oid: walkoid}, req_type, &block)
+        value_is_symbol = next_get.values[0].is_a?(::Symbol)
+        if req_type == :get
+          req_type = :getnext
+          next if value_is_symbol
         end
+        walkoid = next_get.keys.last
+        break if !OID.parent?(oid, walkoid) || value_is_symbol
+        output.merge!(next_get)
       end
+      output
     end
 
     # Perform a SNMP GETBULK Request (performs multiple GETNEXT)
@@ -120,21 +108,22 @@ module NETSNMP
     #  end
     #end
 
-    # Perform a SNMP SET Request
+    private
+
+    # Performs any SNMP Request returning {oid => value}
+    #
+    # will yield the PDU if block is given
     #
     # @see {NETSNMP::Varbind#new}
     #
-    def set(oid_opts)
-      request = @session.build_pdu(:set, oid_opts)
-      response = handle_retries { @session.send(request) }
-      yield response if block_given? 
-      response.varbinds.map(&:value)
+    def xet(oid_opts, type = :get)
+      pdu = handle_retries { @session.request(type, oid_opts) }
+      yield pdu if block_given?
+      Hash[pdu.varbinds.map{|v| [v.oid, v.value] }]
     end
 
-
-    private
-
-    # Handles timeout errors by reissuing the same pdu until it runs out or retries. 
+    # Handles timeout errors by reissuing the same pdu until it runs out or retries.
+    #
     def handle_retries
       retries = @retries
       begin
@@ -145,5 +134,6 @@ module NETSNMP
         retry
       end
     end
+
   end
 end
